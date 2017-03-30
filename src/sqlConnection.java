7dd811e7407b21d76d5a9b197967a7e7b933ad0d
Alex <alex.rombach@gmail.com>
2017-03-30 14:19:43 -0400

    lol jk we can just all do the same 123 password - nick show us later

---
+++ src/sqlConnection.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class sqlConnection {
+	Connection conn = null;
+	public static Connection dbConnector()
+	{
+		try{
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3306/cit345groupproject?autoReconnect=true&useSSL=false";
+			String username = "root";
+			String password = "Password123";
+			
+			Class.forName(driver);
+			
+			Connection conn = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected");
+			return conn;
+		
+			}catch(Exception e) {System.out.println(e);}
+	
+	return null;
+	}
+}
